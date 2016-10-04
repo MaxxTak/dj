e10b13e69e433ca1a01243ae9ee8d6993b8ad800
MaxxTak <thiagoakira@live.com>
2016-10-04 23:15:35 +0000

    modificado: mysite/blog/admin.py 	modificado: mysite/blog/admin.pyc 	modificado: mysite/blog/urls.py 	modificado: mysite/blog/urls.pyc 	modificado: mysite/blog/views.py 	modificado: mysite/blog/views.pyc 	modificado: mysite/db.sqlite3 	modificado: mysite/mysite/urls.py 	modificado: mysite/mysite/urls.pyc 	mysite/blog/templates/ 	../mysite/

--- env/mysite/blog/views.py
+++ env/mysite/blog/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def post_list(request):
+    return render(request, 'blog/post_list.html',{})
